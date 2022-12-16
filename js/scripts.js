21054923b07a609cbc3b11e34ca41b394e2fdc73
marek_k <[200~spaceshiftst@gmail.com>
2022-12-16 01:28:27 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+'use strict';
+const div = document.querySelector('div');
+div.innerHTML = `
+<ol>
+    <li>Adam</li>
+    <li>Paweł</li>
+    <li>Andrzej</li>
+    <li>Ilona</li>
+    <li>Jagoda</li>
+    <li>Lucyna</li>
+    <li>Mikołaj</li>
+    <li>Kajetan</li>
+    <li>Hubert</li>
+    <li>Mirosław</li>
+</ol>
+`
+console.log(div.innnerHTML)
